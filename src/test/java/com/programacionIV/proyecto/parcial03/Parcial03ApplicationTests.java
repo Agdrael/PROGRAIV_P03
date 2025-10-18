03a14e9f2ba311f30d3f6708eeed6b0ed37c0545
Agdrael <rodrigojoserivera2016@gmail.com>
2025-10-18 16:28:08 -0600

    agregando base del proyecto

---
+++ src/test/java/com/programacionIV/proyecto/parcial03/Parcial03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programacionIV.proyecto.parcial03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcial03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
